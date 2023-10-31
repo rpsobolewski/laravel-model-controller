2f2e0012edac0a266f906740742c35055f84ae60
rpsobolewski <rpsobolewski@gmail.com>
2023-10-31 12:19:48 +0100

    work on Movie

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\PageController as PageController;
+
+Route::get('/', [PageController::class, 'home'])->name('home');;
